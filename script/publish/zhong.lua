a3fcdc81d51b90fd6dfd82c6a75b5a83d89014d7
huangjx <zhandouxiaojiji@gmail.com>
2018-09-09 00:12:58 +0800

    test publish

--- script/publish/zhong.lua
+++ script/publish/zhong.lua
@@ -5,10 +5,22 @@ conf.remote_host = "zhong@dev.zhongfq.com"
 conf.remote_port = 22
 conf.remote_path = "/mnt/dev/home/zhong/server/monitor"
 conf.etcname = "monitord"
+conf.clustername = "monitor"
+conf.cluster = {
+    monitor = "127.0.0.1:19900",
+    share   = "127.0.0.1:19901",
+}
+conf.mongo = {
+    host = "127.0.0.1",
+    name = "test",
+    port = 19999,
+}
+conf.redis = {
+    host = "127.0.0.1",
+    port = 19998,
+}
 conf.webconsole = {
     host = "dev.zhongfq.com",
-    port = 8751,
+    port = 9999,
 }
-conf.alert = true
-
 return conf
